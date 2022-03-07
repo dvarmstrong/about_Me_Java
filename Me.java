570478a3d84cb91705debcc819d4d4684c1a965f
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-03-07 14:49:18 -0500

    First commit for java

---
+++ Me.java
@@ -0,0 +1,9 @@
+public class Me {
+
+    public static void main(String[] args) {
+        System.out.println("My name is  Dimitri");
+        System.out.println("I am 43 years old");
+        System.out.println("My hometown is Charleston SC");
+
+    }
+}
